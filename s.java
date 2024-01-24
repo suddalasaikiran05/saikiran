3b35c967125d7df9301b78c36d99f9ef3617c4d8
suddalasaikiran05 <suddalasaikiran05@gmail.com>
2024-01-24 09:59:55 +0530

    sfile

---
+++ s.java
@@ -0,0 +1,46 @@
+class Hi
+{
+  public static void main(String args[])
+   {
+      int a=40,b=10,c=20,d=0;
+     
+      if(a>b) 
+      {
+       d=a;
+       a=b;
+       b=d;
+      }
+
+   
+         if(b>c) 
+       {
+        d=b;
+        b=c;
+        c=d; 
+         
+      }
+         if (a>c)
+       {
+         d=c;
+         c=b;
+         b=c;
+      }
+     
+      System.out.println("a="+a);
+      System.out.println("b="+b);
+      System.out.println("c="+c);
+  }
+} 
+
+
+     
+
+     
+
+
+    
+     
+
+
+
+      
